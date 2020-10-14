ba7fbe73adbd757a7c15bcae21fa35f681447a0f
Nyland Sidifall <npsidifall@aggies.ncat.edu>
2020-10-13 21:40:58 -0400

    Code Up to Date

---
+++ cmake-build-debug/CMakeFiles/AggieShell_JoNy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AggieShell_JoNy"
+  "AggieShell_JoNy.pdb"
+  "CMakeFiles/AggieShell_JoNy.dir/shell.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AggieShell_JoNy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
